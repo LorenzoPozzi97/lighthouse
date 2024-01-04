3c82c18fd1ab774b0b4fe32696814cb7c66d4399
Lorenzo Pozzi <lpozzi@pcopernico.it>
2024-01-04 11:59:21 +0100

    Small fixes

--- lighthouse/bidimensional_graphs.py
+++ lighthouse/bidimensional_graphs.py
@@ -7,6 +7,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
+# 'Quadro P1000-3.94-15.35-12-llama-2-7b-chat.Q2_K.gguf'
+
 def parse_arguments():
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(
@@ -14,11 +16,11 @@ def parse_arguments():
         description='Generate 2D interactive graphs of LLM inference performance.',
     )
     parser.add_argument('-x', '--xaxes', action='store', default='GPU Layers', choices=['GPU Layers', 'Threads', 'Batch Threads'], type=str, help="x axes parameter")
+    parser.add_argument('--node_anchor', action='store', type=str, required=True, help="anchor value for current machine and model")
     parser.add_argument('--threads_anchor', action='store', default=-1, type=int, help="anchor value for number of threads")
     parser.add_argument('--batch_threads_anchor', action='store', default=-1, type=int, help="anchor value for number of batch threads")
     parser.add_argument('--gpu_anchor', action='store', default=-1, type=float, help="anchor value for number of threads")
 
-    
     return parser.parse_args()
 
 def main():
@@ -29,8 +31,8 @@ def main():
     threads_filter = df['Threads'] == args.threads_anchor if args.threads_anchor!=-1 else df['Threads'] == df['Threads']
     batch_threads_filter = df['Batch Threads'] == args.batch_threads_anchor if args.batch_threads_anchor!=-1 else df['Batch Threads'] == df['Batch Threads']
     gpu_filter = df['GPU Layers'] == args.gpu_anchor if args.gpu_anchor!=-1 else df['GPU Layers'] == df['GPU Layers']
-    
-    filtered_df = df[(df['Node ID']=='Quadro P1000-3.94-15.35-12-llama-2-7b-chat.Q2_K.gguf') & 
+
+    filtered_df = df[(df['Node ID']==args.node_anchor) & 
                     threads_filter &
                     batch_threads_filter &
                     gpu_filter
