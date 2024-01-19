bbae0902442ead3a0efdad6fc6fcd771f44cd43d
Lorenzo Pozzi <lpozzi@pcopernico.it>
2024-01-19 12:12:52 +0100

    Added venv setup file

---
+++ setup.sh
@@ -0,0 +1,27 @@
+# Create a virtual environment
+python3 -m venv .lighthouse
+
+# Activate the virtual environment
+source .lighthouse/bin/activate
+
+# Install packages
+pip install pandas==2.1.4
+pip install nltk==3.8.1
+pip install plotly==5.18.0
+pip install psutil==5.9.7
+pip install torch==2.1.2
+pip install torchvision==0.16.2
+pip install torchaudio==2.1.2
+
+# GGUF pkgs
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip install llama-cpp-python==0.2.26 --no-cache-dir
+
+# GPTQ pkgs
+pip install accelerate==0.26.1
+pip install optimum==1.16.1
+pip install auto-gptq==0.6.0
+pip install ninja==1.11.1.1
+MAX_JOBS=4 pip install flash-attn==2.4.2 --no-build-isolation
+pip install transformers==4.36.2
+
+echo "Virtual environment and packages installed successfully."
